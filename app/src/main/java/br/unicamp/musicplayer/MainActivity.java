086e273f5a514e9e69b77495fc74cd4d2b37dbb9
7863752434 <32243291+7863752434@users.noreply.github.com>
2019-11-27 09:22:40 -0300

    aa

--- app/src/main/java/br/unicamp/musicplayer/MainActivity.java
+++ app/src/main/java/br/unicamp/musicplayer/MainActivity.java
@@ -45,10 +45,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null)
-            startActivity(new Intent(this, Musicas.class).putExtra("acc", account));
-
         btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
